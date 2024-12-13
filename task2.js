14fbcd2a01675b2f12b99d1773226930a54ec379
Valeria <varivodavaleria715@gmail.com>
2024-12-13 16:57:57 +0100

    task2.js

---
+++ task2.js
@@ -0,0 +1,7 @@
+let name1 = "pavel";
+let name2 = "olya";
+let equal = "hello " + name1 + " " + name2;//об'єднуємо
+console.log(equal);//виводимо в консоль
+
+let greeting = `Привіт, ${name1} та ${name2}!`;
+console.log(greeting);
